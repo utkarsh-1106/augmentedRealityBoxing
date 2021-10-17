2b86ff3802be4027a584d1f5a87456ad08e1ea93
Utkarsh Sinha <43957190+utkarsh-1106@users.noreply.github.com>
2021-10-17 19:06:07 +0530

    Add files via upload

---
+++ PlayBtnScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBtnScript : MonoBehaviour
+{
+    bool isBoxing = false;
+    public GameObject player;
+    public GameObject videoPlane;
+    Animator anim;
+
+    void Start()
+    {
+        anim = player.GetComponent<Animator>();
+        videoPlane.SetActive(false);
+    }
+
+    public void PlayAnim()
+    {
+        Debug.Log("PLAY clicked");
+        isBoxing = true;
+
+        anim.SetBool("IsBoxing", isBoxing);
+
+        //Play video
+        videoPlane.SetActive(true);
+    }
+
+    public void PauseAnim()
+    {
+        Debug.Log("PAUSE clicked");
+        isBoxing = false;
+
+        anim.SetBool("IsBoxing", isBoxing);
+
+        //Pause video
+        videoPlane.SetActive(false);
+    }
+}
